50be14cd7923a604b2af480b48777c077e58b9cf
Dima Sudoma <dsudoma@intelvision.pro>
2023-09-30 17:33:19 +0300

    Implement mock document upload

---
+++ DocumentUploader/Data/FileUploadInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace DocumentUploader.Data;
+
+public record FileUploadInfo
+{
+    public string Name { get; set; } = string.Empty;
+    public long SizeInKb { get; set; }
+    public string Type { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Label { get; set; } = string.Empty;
+    public string Link { get; set; } = string.Empty;
+}
